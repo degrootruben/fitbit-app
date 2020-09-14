0d9840dcb3bd512a46385eede55df5c43d24b631
degrootruben <rpg.degroot@gmail.com>
2020-09-14 23:37:00 +0200

    Tried some diffirent things and deleted settings folder

--- app/index.js
+++ app/index.js
@@ -1,1 +1,16 @@
-console.log('Hello world!');
+import document from "document";
+
+let bg = document.getElementById("background");
+let button = document.getElementById("disco");
+let disco = false;
+
+button.addEventListener("click", () => {
+    disco = !disco;
+});
+
+if (disco) {
+    setInterval(() => {
+            const randomColor = Math.floor(Math.random()*16777215).toString(16);
+            bg.style.fill = "#" + randomColor;
+    }, 500);
+}
